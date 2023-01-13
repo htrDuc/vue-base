8d7d3db77248dccc5af49d843d291e1ba32397af
ducht <ducht@smartosc.com>
2023-01-13 16:09:42 +0700

    chore: lint commit message

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from "./App.vue";
 import "./assets/css/index.css";
 import router from "./router/index";
 import { createPinia } from "pinia";
+
 const pinia = createPinia();
 createApp(App).use(router).use(pinia).mount("#app");
